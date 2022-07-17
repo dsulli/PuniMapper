d387aa91e98b3d8df769299ebc73c53ff31c57a9
Deanna Sulli <dtcsulli@gmail.com>
2022-07-16 17:05:09 -0700

    i forgot to build locally

---
+++ dist/_nuxt/static/1658016161/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","IlMheg","\u002FYanxia","\u002FIlMheg","\u002FLabyrinthos","\u002FGarlemald","\u002FUltimaThule","\u002FTheAzimSteppe","\u002FAmhAraeng","\u002FCoerthasWesternHighlands","\u002FCentralThanalan","\u002FLowerLaNoscea","\u002FUpperLaNoscea"]})
